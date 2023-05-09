747fd4f1d4ab2bdd9c26d82e718e1be053e5bae5
robin <robin@192.168.0.20>
2023-05-09 16:20:35 +0200

    Avec Thomas mais on est reparti sur des 0 partout

--- tp_laplace.m
+++ tp_laplace.m
@@ -1,5 +1,5 @@
-%test branche thomas
-format longG
+%test banche thomas
+format longG 
 % Définition du maillage
     % Nombre de noeuds en hauteur
 imax=30;
@@ -35,13 +35,11 @@ T0=278;
 q=410/33; 
 
 % Coefficient intermédiaire
-    % Nombre de Biot supérieur
-Biote=he*dx/k;
-    % Nombre de Biot inférieur
-Bioti=hi*dx/k;
+y=zeros(jmax);
+b=zeros(jmax);
 
 % Paramètre pour la précision et son test
-precision=0.000001;
+precision=0.0001;
 testpr=1;
 testT=1;
 
@@ -50,92 +48,83 @@ testT=1;
 iter=0;
 while (testpr>precision)
   testpr=0;
-    for i = 1:imax
-        % Initialisation du dy avant (dy1) et après (dy2) maille 
-        if (i+1) < (1+imax/2)
-            dy1=(hauteur*4/(imax^2))*(i-1);
-            dy2=(hauteur*4/(imax^2))*(i);
-        elseif i <= (1+imax/2) && (i+1) >= (1+imax/2)
-            dy1=(hauteur*4/(imax^2))*(i-1);
-            dy2=(hauteur*4/(imax^2))*(imax-i);
-        elseif i > (1+imax/2)
-            dy1=(hauteur*4/(imax^2))*(imax-i+1);
-            dy2=(hauteur*4/(imax^2))*(imax-i);
-        end 
-        % dy1 représente la distance entre la maille et celle juste au-dessus (la précédante), pour la première maille, celle distance vaut 0 mais il n'y a pas de précédante, ce n'est donc pas un prblème
-        % dy2 de la même manière est la distance entre le maille et celle au-dessous (la suivante), dy2 pour la dernière maille vaut 0, plus de maille suivante
-        % L'évolution des dy est linéaire croissante jusqu'à la moitié puis décroissante de pente opposé après la moitié 
-        
 
-        for j = 1:jmax
-            testT = T(i, j);
+  % Première ligne, on fixe d'abord pour le premier point puis une boucle
+  % pour les autres
+  i=1;
+  
+  dy2=(hauteur*4/(imax^2))*(i);
+  
+  for j = 1:jmax-1
+      y(j+1)=-(dy2/2*dx)/((dy2/2*dx)*y(j)+((he*dx)/k)+(dy2/dx)+(dx/dy2));
+      b(j+1)=(((dx/dy2)*T(i+1,j))+(((he*dx)/k)*Te)-(dy2/2*dx)*b(j))/((dy2/2*dx)*y(j)+((he*dx)/k)+(dy2/dx)+(dx/dy2));
+  end
+  testT=T(i,jmax);
+  T(i,jmax)=(((dy2/dx)*T(i,jmax-1))+((dx/dy2)*T(i+1,jmax))+(((he*dx)/k)*Te)) /(((he*dx)/k)+(dy2/dx)+(dx/dy2));
+  testpr=max(testpr,abs(T(i,jmax)-testT));
+  for j = jmax:2
+      testT=T(i,j-1);
+      T(i,j-1)=y(j)*T(i,j)+b(j);
+      testpr=max(testpr,abs(T(i,j-1)-testT));
+  end
 
-            if i > 1 && j > 1 && i < imax && j < jmax
-                % Si la cellule n'est pas sur un bord ou un coin
-                T(i,j) = (T(i-1,j)*dx^2*dy2 + T(i+1,j)*dx^2*dy1 + (T(i,j-1)+T(i,j+1))*((dy1+dy2)/2)*dy1*dy2) / (dx^2*dy1 + dx^2*dy2 + dy1^2*dy2 + dy2^2*dy1);
-                %T(i,j) = (T(i-1,j) + T(i+1,j) + T(i,j-1) + T(i,j+1)) / 4;
-            elseif i == 1 && j > 1 && j < jmax
-                % Si la cellule est sur le bord supérieur
-                %T(i,j) = (Biote*Te + T(i+1,j)*dx/dy2 + (T(i,j-1)+T(i,j+1))*dy2/(2*dx)) / (Biote + 2);
 
-                T(i,j)=(((dy2/(2*dx))*T(i,j-1))+((dy2/(2*dx))*T(i,j+1))+((dx/dy2)*T(i+1,j))+(((he*dx)/k)*Te)) /(((he*dx)/k)+(dy2/dx)+(dx/dy2));
-         
-                %T(i,j) = (Ti + T(i+1,j) + T(i,j-1) + T(i,j+1)) / 4;
-            elseif i == imax && j > 1 && j < jmax
-                % Si la cellule est sur le bord inférieur
-                %T(i,j) = (Bioti*Ti + T(i-1,j)*dx/dy1 + (T(i,j-1)+T(i,j+1))*dy1/(2*dx)) / (Bioti + 2);
+   
+  % Toutes les ligne entre la première et la dernière
+  for i = 2:imax-1
+      % Initialisation du dy avant (dy1) et après (dy2) maille 
+      if (i+1) < (1+imax/2)
+          dy1=(hauteur*4/(imax^2))*(i-1);
+          dy2=(hauteur*4/(imax^2))*(i);  
+      elseif i <= (1+imax/2) && (i+1) >= (1+imax/2)  
+          dy1=(hauteur*4/(imax^2))*(i-1);          
+          dy2=(hauteur*4/(imax^2))*(imax-i);        
+      elseif i > (1+imax/2)            
+          dy1=(hauteur*4/(imax^2))*(imax-i+1);            
+          dy2=(hauteur*4/(imax^2))*(imax-i);        
+      end 
+      % dy1 représente la distance entre la maille et celle juste au-dessus (la précédante), pour la première maille, celle distance vaut 0 mais il n'y a pas de précédante, ce n'est donc pas un prblème
+      % dy2 de la même manière est la distance entre le maille et celle au-dessous (la suivante), dy2 pour la dernière maille vaut 0, plus de maille suivante
+      % L'évolution des dy est linéaire croissante jusqu'à la moitié puis décroissante de pente opposé après la moitié 
+      for j = 1:jmax-1      
+          y(j+1)=-(((dy1+dy2)/2)*dy1*dy2)/((((dy1+dy2)/2)*dy1*dy2)*y(j)+dx^2*dy1 + dx^2*dy2 + dy1^2*dy2 + dy2^2*dy1);      
+          b(j+1)=(T(i-1,j)*dx^2*dy2 + T(i+1,j)*dx^2*dy1-(((dy1+dy2)/2)*dy1*dy2)*b(j))/((((dy1+dy2)/2)*dy1*dy2)*y(j)+dx^2*dy1 + dx^2*dy2 + dy1^2*dy2 + dy2^2*dy1);
+      end
+      testT=T(i,jmax);
+      T(i,j) = (T(i-1,j)*dx^2*dy2 + T(i+1,j)*dx^2*dy1 + T(i,j-1)*(dy1+dy2)*dy1*dy2) / (dx^2*dy1 + dx^2*dy2 + dy1^2*dy2 + dy2^2*dy1);
+      testpr=max(testpr,abs(T(i,jmax)-testT));
+      for j = jmax:2
+          testT=T(i,j-1);
+          T(i,j-1)=y(j)*T(i,j)+b(j);
+          testpr=max(testpr,abs(T(i,j-1)-testT));
+      end
+  end
 
-                T(i,j)=(((dy1/(2*dx))*T(i,j-1))+((dy1/(2*dx))*T(i,j+1))+((dx/dy1)*T(i-1,j))+(((hi*dx)/k)*Ti)) /(((hi*dx)/k)+(dy1/dx)+(dx/dy1));
 
-                %T(i,j) = (T(i-1,j) + Te + T(i,j-1) + T(i,j+1)) / 4;
-            elseif j == 1 && i > 1 && i < imax
-                % Si la cellule est sur le bord gauche
-                T(i,j) = (T(i-1,j)*dx^2*dy2 + T(i+1,j)*dx^2*dy1 + T(i,j+1)*(dy1+dy2)*dy1*dy2) / (dx^2*dy1 + dx^2*dy2 + dy1^2*dy2 + dy2^2*dy1);
-                %T(i,j) = (T(i-1,j) + T(i+1,j) + 2*T(i,j+1)) / 4;
-            elseif j == jmax && i > 1 && i < imax
-                % Si la cellule est sur le bord droit
-                T(i,j) = (T(i-1,j)*dx^2*dy2 + T(i+1,j)*dx^2*dy1 + T(i,j-1)*(dy1+dy2)*dy1*dy2) / (dx^2*dy1 + dx^2*dy2 + dy1^2*dy2 + dy2^2*dy1);
-                %T(i,j) = (T(i-1,j) + T(i+1,j) + 2*T(i,j-1)) / 4;
-            elseif i == 1 && j == 1
-                % Si la cellule est dans le coin supérieur gauche
-                %T(i,j) = (Biote*Te + T(i+1,j)*dx/dy2 + T(i,j+1)*dy2/dx) / (Biote + 2);
 
-                T(i,j)=(((dy2/dx)*T(i,j+1))+((dx/dy2)*T(i+1,j))+(((he*dx)/k)*Te)) /(((he*dx)/k)+(dy2/dx)+(dx/dy2));
-
-                %T(i,j) = (Ti + T(i+1,j) + 2*T(i,j+1)) / 4;
-            elseif i == 1 && j == jmax
-                % Si la cellule est dans le coin supérieur droit
-                %T(i,j) = (Biote*Te + T(i+1,j)*dx/dy2 + T(i,j-1)*dy2/dx) / (Biote + 2);
-
-                T(i,j)=(((dy2/dx)*T(i,j-1))+((dx/dy2)*T(i+1,j))+(((he*dx)/k)*Te)) /(((he*dx)/k)+(dy2/dx)+(dx/dy2));
-
-                %T(i,j) = (Ti + T(i+1,j) + 2*T(i,j-1)) / 4;
-            elseif i == imax && j == 1
-                % Si la cellule est dans le coin inférieur gauche
-                %T(i,j) = (Bioti*Ti + T(i-1,j)*dx/dy1 + T(i,j+1)*dy1/dx + q/k) / (Bioti + 2);
-
-                T(i,j)=(((dy1/dx)*T(i,j+1))+((dx/dy1)*T(i-1,j))+(((hi*dx)/k)*Ti+(q/k))) /(((hi*dx)/k)+(dy1/dx)+(dx/dy1));
-
-                %T(i,j) = (T(i-1,j) + Te + 2*T(i,j+1) + q) / 4;
-            elseif i == imax && j == jmax
-                % Si la cellule est dans le coin inférieur droit
-                %T(i,j) = (Bioti*Ti + T(i-1,j)*dx/dy1 + T(i,j-1)*dy1/dx) / (Bioti + 2);
-
-                T(i,j)=(((dy1/dx)*T(i,j-1))+((dx/dy1)*T(i-1,j))+(((hi*dx)/k)*Ti)) /(((hi*dx)/k)+(dy1/dx)+(dx/dy1));
-
-                %T(i,j) = (T(i-1,j) + Te + 2*T(i,j-1)) / 4;
-            end
-
-            if i == 1
-                % On regarde les températures sur la surface éxtérieur de
-                % la vitre
-                Tpe(1,j)=T(i,j);
-            end
-
-            testpr=max(testpr,abs(T(i,j)-testT));
-        end
-    end
-    iter=iter+1;
+  % La dernière ligne
+  i=imax;
+  
+  dy1=(hauteur*4/(imax^2))*(imax-i+1);  
+  % Comme il y a une source, le terme b(2) doit être initialisé au
+  % préalable
+  j=1;
+  y(j+1)=-(dy1/2*dx)/((dy1/2*dx)*y(j)+((hi*dx)/k)+(dy1/dx)+(dx/dy1));
+  b(j+1)=(((dx/dy1)*T(i-1,j))+(((hi*dx)/k)*Ti+(q/k))-(dy1/2*dx)*b(j))/((dy1/2*dx)*y(j)+((hi*dx)/k)+(dy1/dx)+(dx/dy1));
+  for j = 2:jmax-1
+      y(j+1)=-(dy1/2*dx)/((dy1/2*dx)*y(j)+((hi*dx)/k)+(dy1/dx)+(dx/dy1));
+      b(j+1)=(((dx/dy1)*T(i-1,j))+(((hi*dx)/k)*Ti)-(dy1/2*dx)*b(j))/((dy1/2*dx)*y(j)+((hi*dx)/k)+(dy1/dx)+(dx/dy1));
+  end
+  testT=T(i,jmax);
+  T(i,jmax)=(((dy1/dx)*T(i,jmax-1))+((dx/dy1)*T(i-1,jmax))+(((hi*dx)/k)*Ti)) /(((hi*dx)/k)+(dy1/dx)+(dx/dy1));
+  testpr=max(testpr,abs(T(i,jmax)-testT));
+  for j = jmax:2
+      testT=T(i,j-1);
+      T(i,j-1)=y(j)*T(i,j)+b(j);
+      testpr=max(testpr,abs(T(i,j-1)-testT));
+  end
+  
+  iter=iter+1;
 end
 
 % Affichage de la température sur la vitre extérieur
@@ -147,7 +136,7 @@ colormap(jet); %choix de la palette de couleur : "jet"
 imagesc(T); %affichage sans la grille
 %contourf(T); %affichage avec les courbes de températures
 colorbar;
-axis equal; %mettre la même echelle pour les deux axes
+%axis equal; %mettre la même echelle pour les deux axes
 
 % Vérification de la température minimum
 if T(1,jmax) >= T0 %T(1,jmax) represente la T° en haut à droite, car 1ère ligne et jème colonne
